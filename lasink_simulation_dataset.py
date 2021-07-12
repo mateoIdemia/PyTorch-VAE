bb9656a2bb420fc5e3c0ac4b114219039bcf456d
mateoIdemia <78589454+mateoIdemia@users.noreply.github.com>
2021-07-12 16:02:20 +0200

    create lasink_simulation_dataset

---
+++ lasink_simulation_dataset.py
@@ -0,0 +1,23 @@
+import glob
+from PIL import Image
+from torch.utils.data import Dataset
+import torch
+import random
+import numpy as np
+
+class LasinkSimulation(Dataset):
+    def __init__(self, folder, transform=None):
+        self.transforms=transform
+
+        self.imgs = glob.glob(folder+'//*')
+
+    def __getitem__(self, index):
+        
+        img = Image.open(self.imgs[index])
+        if self.transforms:
+            img = self.transforms(img)
+
+        return img,  0
+
+    def __len__(self):
+        return len(self.imgs)
