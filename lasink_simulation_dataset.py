61fc06f281f7d3a05b305f340ca020b637ec3055
mateoIdemia <78589454+mateoIdemia@users.noreply.github.com>
2021-07-13 10:11:00 +0200

    split train / test

--- lasink_simulation_dataset.py
+++ lasink_simulation_dataset.py
@@ -7,12 +7,18 @@ import numpy as np
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-
 class LasinkSimulation(Dataset):
-    def __init__(self, folder, transform=None):
+    def __init__(self, folder, train=True, transform=None):
         self.transforms=transform
 
         self.imgs = glob.glob(folder+'//*')
+        random.seed(10)
+        random.shuffle(self.imgs)
+        if train:
+            self.imgs = self.imgs[:int(len(self.imgs)*0.85)]
+        else:
+            self.imgs = self.imgs[int(len(self.imgs)*0.85:)]
+
 
     def __getitem__(self, index):
         
